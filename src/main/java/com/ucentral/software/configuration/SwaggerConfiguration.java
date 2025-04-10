fcb4a88c28e3fcd4eec209b64aad0cd755ac03e2
juan poveda <poved4@gmail.com>
2025-04-10 11:57:59 -0500

    feat: configure Swagger OpenAPI documentation

---
+++ src/main/java/com/ucentral/software/configuration/SwaggerConfiguration.java
@@ -0,0 +1,33 @@
+package com.ucentral.software.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class SwaggerConfiguration {
+
+  @Value("${spring.application.name}")
+  private String appName;
+
+  @Value("${spring.application.version}")
+  private String appVersion;
+
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI().info(
+      new Info()
+        .title(appName)
+        .version(appVersion)
+        .description("")
+        .termsOfService("http://swagger.io/terms")
+        .license(new License().name("Apache 2.0").url("http://springdoc.org"))
+    );
+
+  }
+
+}
